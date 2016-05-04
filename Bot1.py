87939893a9a47852e5e7589fbd99dd5c22a54e48
mickeyjk <michael@Michaels-MacBook-Air.local>
2016-05-03 20:44:53 -0400

    Barebones working version
    Basic functionality is all there, now to make edits to make formatting
    look nicer, more functionality

--- Bot1.py
+++ Bot1.py
@@ -1,5 +1,6 @@
 import pprint
 import time
+import anapioficeandfire
 import praw
 from prawoauth2 import PrawOAuth2Mini
 
@@ -11,15 +12,16 @@ from settings import scopes, user_agent
 reddit_client = praw.Reddit(user_agent=user_agent)
 oauth_helper = PrawOAuth2Mini(reddit_client, app_key=app_key, app_secret=app_secret, access_token=access_token, scopes=scopes, refresh_token=refresh_token)
 
-subreddit = reddit_client.get_subreddit('circlejerk')
-
 already_checked = []
 
-logFile = "log"
+logFile = "log.txt"
+
+api = anapioficeandfire.API()
+
 
 def writeToLog(fileName, id):
     file = open(fileName, 'a')
-    file.write(id)
+    file.write(id + '\n')
     file.close()
 
 def readLog(fileName):
@@ -32,13 +34,82 @@ def readLog(fileName):
 def contains_bot_text(c):
     text = c.body
     tokens = text.split()
-    if "ARefOfIceAndFire" in tokens:
+    if tokens[0] == "ARefBotOfIceAndFire":
         return True
+def get_character_data(c):
+    response = ''
 
-def print_api_info(c):
-    c.reply("Jon Snuu")
-    print("something happened!\n")
+    #NAME
+    response += '## **' + c.name + '**    \n'
 
+    ##TTITLES
+    if c.titles:
+        response += '### ';
+        i = 0
+        while i != len(c.titles)-1:
+            response += c.titles[i] + ', '
+            i += 1
+        response += c.titles[i]
+    ##ALIASES
+    if c.aliases:
+        response += ', '
+        i = 0
+        while i != len(c.aliases)-1:
+            response += c.aliases[i] + ', '
+            i += 1
+        response += c.aliases[i]
+    response += '    \n'
+    response += 'Born: ' + c.born + '    \n'
+    response += 'Died: ' + c.died + '    \n'
+    response += 'Father: ' + c.father + '    \n'
+    response += 'Mother: ' + c.mother + '    \n'
+    response += 'Spouse: ' + c.spouse + '    \n'
+
+    ##ALLEGIANCES
+    if c.allegiances:
+        response += 'Allegiances: '
+        for url in c.allegiances:
+            tokens = url.split('/')
+            id = int(tokens[-1])
+            house = api.get_house(id=id)
+            response += house.name + ', '
+        response = response [:-2] + '    \n'
+
+    ##BOOKS
+    if c.books:
+        response += "Appears in: "
+        for url in c.books:
+            tokens = url.split('/')
+            id = int(tokens[-1])
+            book = api.get_book(id=id)
+            response += book.name + ', '
+        response = response[:-2] + '    \n'
+
+    if c.playedBy:
+        response += "Played by: "
+        for name in c.playedBy:
+            response += name + ', '
+        response = response[:-2] + '    \n'
+    response += '***\n\n'
+    return response
+
+
+def get_data(characters):
+    response = '# ARefBotOfIceAndFire v1.0\n\n'
+    for c in characters:
+        response += get_character_data(c)
+    return response
+
+
+def print_api_info(comment, query):
+    characters = api.get_characters(name=query)
+    reply = get_data(characters)
+    comment.reply(reply)
+    print("Comment replied: \n" + reply)
+
+def get_query(text):
+    q =  text.replace('ARefBotOfIceAndFire ', '', 1)
+    return q
 
 def inner_loop():
     oauth_helper.refresh()
@@ -46,10 +117,11 @@ def inner_loop():
     for c in praw.helpers.comment_stream(reddit_client, 'circlejerk'):
         if c.id not in already_checked and contains_bot_text(c):
             print("\n\n\n\n\n\n")
-            print(c.author)
             print(c.submission)
+            print(c.author)
             print(c.body)
-            print_api_info(c)
+            query = get_query(c.body)
+            print_api_info(c, query)
             already_checked.append(c.id)
             writeToLog(logFile, c.id)
             time.sleep(540)
